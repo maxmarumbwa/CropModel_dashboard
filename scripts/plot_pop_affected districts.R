fb4b53b8553db6015b42e1062bfc0dd1ba8e9df7
faraigit <faraimax@gmail.com>
2022-05-08 14:13:47 +0200

    fixed mapviw display

--- scripts/plot_pop_affected districts.R
+++ scripts/plot_pop_affected districts.R
@@ -10,6 +10,7 @@ library(ggplot2) # tidyverse data visualization package
 
 # pop affected data
 pop_affe <- read.csv(here("data","csv","pop_affe.csv"))
+prj <- CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
 vuln <- read_sf(here("data","shp","vuln.shp"),crs = 4326)
 
 # Rename the pop affected to match shp
@@ -25,5 +26,14 @@ tm_shape(pop_affected) + tm_polygons(facets) +
 #Using mapview to create maps
 #mapview(world_coffee1) #
 mapview(pop_affected, zcol = "Pop_affected_2004.05")
+
+
+world_coffee = left_join(world, coffee_data, by = "name_long")
+facets = c("coffee_production_2016", "coffee_production_2017")
+tm_shape(world_coffee) + tm_polygons(facets) +
+  tm_facets(nrow = 1, sync = TRUE)
+
+
+
 
 
